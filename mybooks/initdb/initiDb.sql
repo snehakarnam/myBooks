16e7270fae636b853e2ebca0726f4e3fad766373
snehakarnam <sne.karnam@gmail.com>
2024-08-31 14:48:42 +0530

    added DB scripts

--- mybooks/initdb/initiDb.sql
+++ mybooks/initdb/initiDb.sql
@@ -12,18 +12,18 @@ CREATE TABLE IF NOT EXISTS public.vendor
     bank_details character varying COLLATE pg_catalog."default" NOT NULL,
     created_at timestamp with time zone NOT NULL DEFAULT now(),
     updated_at timestamp with time zone NOT NULL,
-    CONSTRAINT vendor_pkey PRIMARY KEY (vendor_id)
-)
-TABLESPACE pg_default;
+                             CONSTRAINT vendor_pkey PRIMARY KEY (vendor_id)
+    )
+    TABLESPACE pg_default;
 
 ALTER TABLE IF EXISTS public.vendor
     OWNER to postgres;
 
--- Table: public.customer_entity
+-- Table: public.customer
 
--- DROP TABLE IF EXISTS public.customer_entity;
+-- DROP TABLE IF EXISTS public.customer;
 
-CREATE TABLE IF NOT EXISTS public.customer_entity
+CREATE TABLE IF NOT EXISTS public.customer
 (
     customer_id character varying COLLATE pg_catalog."default" NOT NULL,
     name character varying COLLATE pg_catalog."default" NOT NULL,
@@ -32,18 +32,18 @@ CREATE TABLE IF NOT EXISTS public.customer_entity
     address character varying COLLATE pg_catalog."default",
     created_at timestamp with time zone NOT NULL,
     updated_at timestamp with time zone NOT NULL,
-    CONSTRAINT customer_entity_pkey PRIMARY KEY (customer_id)
-)
-TABLESPACE pg_default;
+    CONSTRAINT customer_pkey PRIMARY KEY (customer_id)
+    )
+    TABLESPACE pg_default;
 
-ALTER TABLE IF EXISTS public.customer_entity
+ALTER TABLE IF EXISTS public.customer
     OWNER to postgres;
 
--- Table: public.product_group_entity
+-- Table: public.product_group
 
--- DROP TABLE IF EXISTS public.product_group_entity;
+-- DROP TABLE IF EXISTS public.product_group;
 
-CREATE TABLE IF NOT EXISTS public.product_group_entity
+CREATE TABLE IF NOT EXISTS public.product_group
 (
     product_group_id character varying COLLATE pg_catalog."default" NOT NULL,
     vendor_id character varying COLLATE pg_catalog."default" NOT NULL,
@@ -51,23 +51,23 @@ CREATE TABLE IF NOT EXISTS public.product_group_entity
     description text COLLATE pg_catalog."default",
     created_at timestamp with time zone NOT NULL,
     updated_at timestamp with time zone NOT NULL,
-    CONSTRAINT product_group_entity_pkey PRIMARY KEY (product_group_id),
+    CONSTRAINT product_group_pkey PRIMARY KEY (product_group_id),
     CONSTRAINT fk_product_group_vendor FOREIGN KEY (vendor_id)
-        REFERENCES public.vendor (vendor_id) MATCH SIMPLE
-        ON UPDATE NO ACTION
-        ON DELETE NO ACTION
-        NOT VALID
-)
-TABLESPACE pg_default;
+    REFERENCES public.vendor (vendor_id) MATCH SIMPLE
+    ON UPDATE NO ACTION
+    ON DELETE NO ACTION
+    NOT VALID
+    )
+    TABLESPACE pg_default;
 
-ALTER TABLE IF EXISTS public.product_group_entity
+ALTER TABLE IF EXISTS public.product_group
     OWNER to postgres;
 
--- Table: public.recurring_plans_entity
+-- Table: public.recurring_plans
 
--- DROP TABLE IF EXISTS public.recurring_plans_entity;
+-- DROP TABLE IF EXISTS public.recurring_plans;
 
-CREATE TABLE IF NOT EXISTS public.recurring_plans_entity
+CREATE TABLE IF NOT EXISTS public.recurring_plans
 (
     plan_id character varying COLLATE pg_catalog."default" NOT NULL,
     vendor_id character varying COLLATE pg_catalog."default" NOT NULL,
@@ -80,28 +80,28 @@ CREATE TABLE IF NOT EXISTS public.recurring_plans_entity
     day_of_week character varying COLLATE pg_catalog."default",
     day_of_month integer,
     created_at timestamp with time zone NOT NULL,
-    CONSTRAINT recurring_plans_entity_pkey PRIMARY KEY (plan_id),
+                             CONSTRAINT recurring_plans_pkey PRIMARY KEY (plan_id),
     CONSTRAINT fk_recurring_vendor FOREIGN KEY (vendor_id)
-        REFERENCES public.vendor (vendor_id) MATCH SIMPLE
-        ON UPDATE NO ACTION
-        ON DELETE NO ACTION
-        NOT VALID,
+    REFERENCES public.vendor (vendor_id) MATCH SIMPLE
+                         ON UPDATE NO ACTION
+                         ON DELETE NO ACTION
+    NOT VALID,
     CONSTRAINT fk_recurring_product_group FOREIGN KEY (product_group_id)
-        REFERENCES public.product_group_entity (product_group_id) MATCH SIMPLE
-        ON UPDATE NO ACTION
-        ON DELETE NO ACTION
-        NOT VALID
-)
-TABLESPACE pg_default;
+    REFERENCES public.product_group (product_group_id) MATCH SIMPLE
+                         ON UPDATE NO ACTION
+                         ON DELETE NO ACTION
+    NOT VALID
+    )
+    TABLESPACE pg_default;
 
-ALTER TABLE IF EXISTS public.recurring_plans_entity
+ALTER TABLE IF EXISTS public.recurring_plans
     OWNER to postgres;
 
--- Table: public.subscription_entity
+-- Table: public.subscription
 
--- DROP TABLE IF EXISTS public.subscription_entity;
+-- DROP TABLE IF EXISTS public.subscription;
 
-CREATE TABLE IF NOT EXISTS public.subscription_entity
+CREATE TABLE IF NOT EXISTS public.subscription
 (
     subscription_id character varying COLLATE pg_catalog."default" NOT NULL,
     customer_id character varying COLLATE pg_catalog."default" NOT NULL,
@@ -113,33 +113,33 @@ CREATE TABLE IF NOT EXISTS public.subscription_entity
     next_billing_date date NOT NULL,
     created_at timestamp with time zone NOT NULL,
     updated_at timestamp with time zone NOT NULL,
-    CONSTRAINT subscription_entity_pkey PRIMARY KEY (subscription_id),
+    CONSTRAINT subscription_pkey PRIMARY KEY (subscription_id),
     CONSTRAINT fk_subscription_customer FOREIGN KEY (customer_id)
-        REFERENCES public.customer_entity (customer_id) MATCH SIMPLE
-        ON UPDATE NO ACTION
-        ON DELETE NO ACTION
-        NOT VALID,
+    REFERENCES public.customer (customer_id) MATCH SIMPLE
+    ON UPDATE NO ACTION
+    ON DELETE NO ACTION
+    NOT VALID,
     CONSTRAINT fk_subscription_plan FOREIGN KEY (plan_id)
-        REFERENCES public.recurring_plans_entity (plan_id) MATCH SIMPLE
-        ON UPDATE NO ACTION
-        ON DELETE NO ACTION
-        NOT VALID,
+    REFERENCES public.recurring_plans (plan_id) MATCH SIMPLE
+    ON UPDATE NO ACTION
+    ON DELETE NO ACTION
+    NOT VALID,
     CONSTRAINT fk_subscription_vendor FOREIGN KEY (vendor_id)
-        REFERENCES public.vendor (vendor_id) MATCH SIMPLE
-        ON UPDATE NO ACTION
-        ON DELETE NO ACTION
-        NOT VALID
-)
-TABLESPACE pg_default;
+    REFERENCES public.vendor (vendor_id) MATCH SIMPLE
+    ON UPDATE NO ACTION
+    ON DELETE NO ACTION
+    NOT VALID
+    )
+    TABLESPACE pg_default;
 
-ALTER TABLE IF EXISTS public.subscription_entity
+ALTER TABLE IF EXISTS public.subscription
     OWNER to postgres;
 
--- Table: public.invoice_entity
+-- Table: public.invoice
 
--- DROP TABLE IF EXISTS public.invoice_entity;
+-- DROP TABLE IF EXISTS public.invoice;
 
-CREATE TABLE IF NOT EXISTS public.invoice_entity
+CREATE TABLE IF NOT EXISTS public.invoice
 (
     invoice_id character varying COLLATE pg_catalog."default" NOT NULL,
     subscription_id character varying COLLATE pg_catalog."default" NOT NULL,
@@ -152,34 +152,33 @@ CREATE TABLE IF NOT EXISTS public.invoice_entity
     payment_link character varying COLLATE pg_catalog."default",
     created_at timestamp with time zone NOT NULL,
     updated_at timestamp with time zone NOT NULL,
-    CONSTRAINT invoice_entity_pkey PRIMARY KEY (invoice_id),
+                             CONSTRAINT invoice_pkey PRIMARY KEY (invoice_id),
     CONSTRAINT fk_subscription FOREIGN KEY (subscription_id)
-        REFERENCES public.subscription_entity (subscription_id) MATCH SIMPLE
-        ON UPDATE NO ACTION
-        ON DELETE NO ACTION
-        NOT VALID,
+    REFERENCES public.subscription (subscription_id) MATCH SIMPLE
+                         ON UPDATE NO ACTION
+                         ON DELETE NO ACTION
+    NOT VALID,
     CONSTRAINT fk_customer FOREIGN KEY (customer_id)
-        REFERENCES public.customer_entity (customer_id) MATCH SIMPLE
-        ON UPDATE NO ACTION
-        ON DELETE NO ACTION
-        NOT VALID,
+    REFERENCES public.customer (customer_id) MATCH SIMPLE
+                         ON UPDATE NO ACTION
+                         ON DELETE NO ACTION
+    NOT VALID,
     CONSTRAINT fk_vendor FOREIGN KEY (vendor_id)
-        REFERENCES public.vendor (vendor_id) MATCH SIMPLE
-        ON UPDATE NO ACTION
-        ON DELETE NO ACTION
-        NOT VALID
-)
-TABLESPACE pg_default;
+    REFERENCES public.vendor (vendor_id) MATCH SIMPLE
+                         ON UPDATE NO ACTION
+                         ON DELETE NO ACTION
+    NOT VALID
+    )
+    TABLESPACE pg_default;
 
-ALTER TABLE IF EXISTS public.invoice_entity
+ALTER TABLE IF EXISTS public.invoice
     OWNER to postgres;
 
+-- Table: public.notification
 
--- Table: public.notification_entity
+-- DROP TABLE IF EXISTS public.notification;
 
--- DROP TABLE IF EXISTS public.notification_entity;
-
-CREATE TABLE IF NOT EXISTS public.notification_entity
+CREATE TABLE IF NOT EXISTS public.notification
 (
     notification_id character varying COLLATE pg_catalog."default" NOT NULL,
     customer_id character varying COLLATE pg_catalog."default" NOT NULL,
@@ -189,19 +188,19 @@ CREATE TABLE IF NOT EXISTS public.notification_entity
     status character varying COLLATE pg_catalog."default" NOT NULL,  -- Sent, Failed
     sent_date timestamp with time zone NOT NULL,
     created_at timestamp with time zone NOT NULL,
-    CONSTRAINT notification_entity_pkey PRIMARY KEY (notification_id),
+    CONSTRAINT notification_pkey PRIMARY KEY (notification_id),
     CONSTRAINT fk_notification_customer FOREIGN KEY (customer_id)
-        REFERENCES public.customer_entity (customer_id) MATCH SIMPLE
-        ON UPDATE NO ACTION
-        ON DELETE NO ACTION
-        NOT VALID,
+    REFERENCES public.customer (customer_id) MATCH SIMPLE
+    ON UPDATE NO ACTION
+    ON DELETE NO ACTION
+    NOT VALID,
     CONSTRAINT fk_notification_vendor FOREIGN KEY (vendor_id)
-        REFERENCES public.vendor (vendor_id) MATCH SIMPLE
-        ON UPDATE NO ACTION
-        ON DELETE NO ACTION
-        NOT VALID
-)
-TABLESPACE pg_default;
+    REFERENCES public.vendor (vendor_id) MATCH SIMPLE
+    ON UPDATE NO ACTION
+    ON DELETE NO ACTION
+    NOT VALID
+    )
+    TABLESPACE pg_default;
 
-ALTER TABLE IF EXISTS public.notification_entity
+ALTER TABLE IF EXISTS public.notification
     OWNER to postgres;
